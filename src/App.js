881db192df64efa86200151af69cd7c2d6ed167c
Abel Sassu <relinquished@hotmail.it>
2023-12-18 17:14:20 +0100

    css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <div className="App-header">
-                <Button label="Hey Tesoro" />
+                <Button label="ENTRA???" />
                 <Img
                     src="https://www.andreaminini.org/data/andreamininiorg/nc-trapezio-isoscele-esempio-am-2023-2.gif"
                     alt="Non penso proprio"
@@ -14,7 +14,7 @@ function App() {
                     src="https://media.tenor.com/GPgVu42C8gUAAAAM/trapezio.gif"
                     alt="Non penso proprio"
                 />
-                <Button label="Troppo Grosso" />
+                <Button label="Non penso proprioh" />
             </div>
         </div>
     );
